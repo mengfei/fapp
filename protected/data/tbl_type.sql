dd72e731baa7e5b8e55ad0018f151663dc417235
zdsenve <zdsenve@gmail.com>
2012-11-27 00:51:52 +0800

    data数据文件

---
+++ protected/data/tbl_type.sql
@@ -0,0 +1,2 @@
+INSERT INTO "tbl_type" ("id", "type", "name", "addtime") VALUES ('1', 'testtet', '1111ffee', '1352304737');
+INSERT INTO "tbl_type" ("id", "type", "name", "addtime") VALUES ('2', '22test', '地方深v', '1352304750');
