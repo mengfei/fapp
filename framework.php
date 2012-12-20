3cb3cdbe1c5415ccd7245e1ea1bd9a65ce8c1bc8
mengfei <123115756@qq.com>
2012-12-20 20:05:10 +0800

    12-20 framework 9-11

--- framework.php
+++ framework.php
@@ -3,6 +3,8 @@
 defined('APP_PATH') ||
 define('APP_PATH',dirname(__FILE__)."/framework/");
 define('FRAMEWORK_PATH',dirname(__FILE__)."/framework/");
+
+define('APP_DEBUG') or define('APP_DEBUG',true);
 //导入前端控制器
 //include  APP_PATH.'framework//Library/Toper/Core/FrontController.class.php';
 //$frontController = Tp_FrontController::getInstance();
--- framework/controller/IndexController.php
+++ framework/controller/IndexController.php
@@ -1,9 +1,15 @@
 <?php
-class IndexController
+class IndexController extends Controller
 {
     public function index() {
-        $index = new Index();
-        $index->test();
+        //$index = new Index();
+        //$index->test();
+
+		$this->_assign(array(
+			'arr'=>array('test','test2'),
+			'str'=>'it is a str'));
+
+		$this->_display('test');
     }
 }
 ?>
